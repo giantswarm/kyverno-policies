f7228f5b16715a5e0623ffbde7f51b1b2a538731
Marcel Mueller <marcel.mueller1@rwth-aachen.de>
2021-04-20 15:23:09 +0200

    Use makefile

---
+++ Makefile.custom.mk
@@ -0,0 +1,13 @@
+.PHONY: generate
+generate:
+	for i in aws azure common vmware; \
+	do \
+		mkdir -p helm/policies-$$i/templates; \
+		cp -a policies/$$i/. helm/policies-$$i/templates; \
+		for filename in helm/policies-$$i/templates/*.yaml; \
+		do \
+			sed -i 's/{{/üüü/g' $$filename; # replace {{ with placeholder \
+			sed -i 's/}}/{{ `}}` }}/g' $$filename; # replace }} with correct helm substitution \
+			sed -i 's/üüü/{{ `{{` }}/g' $$filename; # replace the placeholder correct helm substition for {{ \
+		done \
+	done
