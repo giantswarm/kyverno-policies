fbedfe4690eb0b4e5a3ff87df7974438563270bb
Thomas Fussell <thomas@fussell.io>
2021-05-04 14:48:53 -0400

    inject proxy env

--- template.sh
+++ template.sh
@@ -6,10 +6,13 @@ for i in aws azure common vmware; do
 
   cp -a policies/$i/. helm/policies-$i/templates
 
-  for filename in helm/policies-$i/templates/*.yaml; do
-    sed -i 's/{{/üüü/g' $filename # replace {{ with placeholder
-    sed -i 's/}}/{{ `}}` }}/g' $filename # replace }} with correct helm substitution
-    sed -i 's/üüü/{{ `{{` }}/g' $filename # replace the placeholder correct helm substition for {{
+  for filename in $(find "helm/policies-$i/templates" -name "*.yaml"); do
+    if [[ $(basename $filename) == "Pod.yaml" ]]; then
+      break
+    fi
+    sed -i '' 's/{{/üüü/g' $filename # replace {{ with placeholder
+    sed -i '' 's/}}/{{ `}}` }}/g' $filename # replace }} with correct helm substitution
+    sed -i '' 's/üüü/{{ `{{` }}/g' $filename # replace the placeholder correct helm substition for {{
   done
 
 done
