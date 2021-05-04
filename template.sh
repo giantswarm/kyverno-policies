65b69b5840ead54caec3429dddead9b856007369
Thomas Fussell <thomas@fussell.io>
2021-05-04 16:32:05 -0400

    fix script, adjust helm template

--- template.sh
+++ template.sh
@@ -7,8 +7,10 @@ for i in aws azure common vmware; do
   cp -a policies/$i/. helm/policies-$i/templates
 
   for filename in $(find "helm/policies-$i/templates" -name "*.yaml"); do
+    echo $filename
     if [[ $(basename $filename) == "Pod.yaml" ]]; then
-      break
+      echo "skip"
+      continue
     fi
     sed -i '' 's/{{/üüü/g' $filename # replace {{ with placeholder
     sed -i '' 's/}}/{{ `}}` }}/g' $filename # replace }} with correct helm substitution
