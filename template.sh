2fecb0e65774edd7d895574d0899f9c234686de6
Thomas Fussell <thomas@giantswarm.io>
2021-05-06 08:42:39 -0400

    improve templating (#20)

--- template.sh
+++ template.sh
@@ -1,15 +1,36 @@
 #!/bin/bash
 set -euo pipefail
 
-for i in aws azure common vmware; do
+placeholder="üüü"
+# shellcheck disable=SC2016
+open_brackets_escaped='{{ `{{` }}'
+# shellcheck disable=SC2016
+close_brackets_escaped='{{ `}}` }}'
+
+replace () {
+  local filename=$1
+  local from=$2
+  local to=$3
+  sed -i.bak "s/$from/$to/g" "$filename" && rm "$filename".bak # https://stackoverflow.com/a/44877280
+}
+
+replace_all () {
+  local filename=$1
+  replace "$filename" '{{' $placeholder
+  replace "$filename" '}}' "$close_brackets_escaped"
+  replace "$filename" $placeholder "$open_brackets_escaped"
+  replace "$filename" '\[\[' '{{'
+  replace "$filename" '\]\]' '}}'
+}
+
+for i in aws azure common kvm vmware; do
   mkdir -p helm/policies-$i/templates
 
   cp -a policies/$i/. helm/policies-$i/templates
 
-  for filename in helm/policies-$i/templates/*.yaml; do
-    sed -i 's/{{/üüü/g' $filename # replace {{ with placeholder
-    sed -i 's/}}/{{ `}}` }}/g' $filename # replace }} with correct helm substitution
-    sed -i 's/üüü/{{ `{{` }}/g' $filename # replace the placeholder correct helm substition for {{
-  done
-
+  # based on https://github.com/koalaman/shellcheck/wiki/SC2044
+  while IFS= read -r -d '' filename
+  do
+    replace_all "$filename"
+  done <   <(find helm/policies-"$i"/templates -name '*.yaml' -print0)
 done
