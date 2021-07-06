aa148f23afa7ad8e4226472f2eb5a47bcfa47a23
Marcel Mueller <marcel.mueller1@rwth-aachen.de>
2021-07-06 19:15:15 +0200

    Improve project structure and make file

--- Makefile.custom.mk
+++ Makefile.custom.mk
@@ -1,13 +1,17 @@
 .PHONY: generate
 generate:
-	./template.sh
+	./hack/template.sh
 
 .PHONY: verify
 verify:
 	@$(MAKE) generate
 	git diff --exit-code
 
-.PHONY: local-setup
-local-setup:
+.PHONY: clean
+clean:
+	./hack/cleanup-local.sh
+
+.PHONY: setup
+setup:
 	@$(MAKE) generate
-	./setup.sh
+	./hack/setup-local.sh
--- hack/setup-local.sh
+++ hack/setup-local.sh
@@ -1,5 +1,6 @@
 kind create cluster --name kyverno-cluster
-kind get kubeconfig --name kyverno-cluster > ./kube.config
+kubectl wait nodes/kyverno-cluster-control-plane --for=condition=ready --timeout=5m > /dev/null
+kind get kubeconfig --name kyverno-cluster > $(pwd)/kube.config
 kubectl create --context kind-kyverno-cluster -f https://raw.githubusercontent.com/kyverno/kyverno/main/definitions/release/install.yaml
 kubectl create --context kind-kyverno-cluster -f https://raw.githubusercontent.com/giantswarm/apiextensions/b9a6bed05850045530eaf8ba7fd01941de6c8525/helm/crds-common/templates/giantswarm.yaml
 kubectl create --context kind-kyverno-cluster -f https://raw.githubusercontent.com/kubernetes-sigs/cluster-api/release-0.3/config/crd/bases/cluster.x-k8s.io_clusters.yaml
