2ae23683c369e11f33d1f189ecb5be7dd60aeb2c
Bricktop <marcel.mueller1@rwth-aachen.de>
2021-07-06 11:36:20 +0200

    Improve local development setup (#40)

---
+++ cleanup-local.sh
@@ -0,0 +1,4 @@
+kubectl delete --context kind-kyverno-cluster awscluster --all
+kubectl delete --context kind-kyverno-cluster cluster --all
+kubectl delete --context kind-kyverno-cluster releases --all
+kubectl delete --context kind-kyverno-cluster AWSClusterRoleIdentity --all
