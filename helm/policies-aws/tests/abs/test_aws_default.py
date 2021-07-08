bc2fe9f37c485838f4c4130238d1dffc4221eef5
Bricktop <marcel.mueller1@rwth-aachen.de>
2021-07-08 12:34:19 +0200

    Add templating for default values (#38)
    * Add templating for default values
    
    * Add testing
    
    * Make work with empty strings
    
    * Fix chart version
    
    * Check for label on object itself
    
    * Generate
    
    * Add handling for dry-run cases
    
    * Simplify common rules
    
    * Rename ClusterPolicy
    
    * Add defaulting for awsclusteridentity
    
    * Update kyverno version
    
    * Fix rules
    
    * Use fixtures instead of functions
    
    * Add some comments
    
    * Add some comments to policies

---
+++ helm/policies-aws/tests/abs/test_aws_default.py
@@ -0,0 +1,72 @@
+import yaml
+from functools import partial
+import time
+import random
+import string
+import ensure
+from textwrap import dedent
+
+from ensure import release
+from ensure import cluster
+from ensure import awscluster
+from ensure import emptyawscluster
+from ensure import emptylabeledawscluster
+from ensure import awsclusterroleidentity
+
+import pytest
+from pytest_kube import forward_requests, wait_for_rollout, app_template
+
+import logging
+LOGGER = logging.getLogger(__name__)
+
+
+@pytest.mark.smoke
+def test_aws_cluster_policy(release, cluster, awscluster) -> None:
+    """
+    test_aws_cluster_policy tests defaulting of an AWSCluster where all required values are empty strings.
+
+    :param release: Release CR which is used by the Cluster.
+    :param cluster: Cluster CR which uses the release and matches the AWSCluster.
+    :param awscluster: AWSCluster CR with empty strings which matches the Cluster CR.
+    """
+    assert awscluster['metadata']['labels']['cluster.x-k8s.io/watch-filter'] == ensure.capa_version
+    assert awscluster['spec']['region'] == "eu-west-1"
+    assert awscluster['spec']['sshKeyName'] == "ssh-key"
+
+
+@pytest.mark.smoke
+def test_aws_cluster_policy_empty(release, cluster, emptyawscluster) -> None:
+    """
+    test_aws_cluster_policy_empty tests defaulting of an AWSCluster where all required values are missing.
+
+    :param release: Release CR which is used by the Cluster.
+    :param cluster: Cluster CR which uses the release and matches the AWSCluster.
+    :param emptyawscluster: Empty AWSCluster CR which matches the Cluster CR.
+    """
+    assert emptyawscluster['metadata']['labels']['cluster.x-k8s.io/watch-filter'] == ensure.capa_version
+    assert emptyawscluster['spec']['region'] == "eu-west-1"
+    assert emptyawscluster['spec']['sshKeyName'] == "ssh-key"
+
+
+@pytest.mark.smoke
+def test_aws_cluster_policy_solo(emptylabeledawscluster) -> None:
+    """
+    test_aws_cluster_policy_solo tests defaulting of an AWSCluster where all required values are missing and no other CRs are given.
+
+    :param emptylabeledawscluster: AWSCluster CR which is empty but has the cluster.x-k8s.io/watch-filter label.
+    """
+    assert emptylabeledawscluster['metadata']['labels']['cluster.x-k8s.io/watch-filter'] == ensure.capa_version
+    assert emptylabeledawscluster['spec']['region'] == "eu-west-1"
+    assert emptylabeledawscluster['spec']['sshKeyName'] == "ssh-key"
+
+
+@pytest.mark.smoke
+def test_aws_cluster_role_identity_policy_solo(awsclusterroleidentity) -> None:
+    """
+    test_aws_cluster_role_identity_policy_solo tests defaulting of an AWSClusterRoleIdentity where all required values are missing and no other CRs are given.
+
+    :param awsclusterroleidentity: AWSClusterRoleIdentity CR with empty strings but has the cluster.x-k8s.io/watch-filter label.
+    """
+    assert awsclusterroleidentity['metadata']['labels']['cluster.x-k8s.io/watch-filter'] == ensure.capa_version
+    assert awsclusterroleidentity['spec']['sourceIdentityRef']['name'] == "default"
+    assert awsclusterroleidentity['spec']['sourceIdentityRef']['kind'] == "AWSClusterControllerIdentity"
