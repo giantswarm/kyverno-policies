7e81bb9a241de753b055bd4acd376164044c6b3a
Marcel Mueller <marcel.mueller1@rwth-aachen.de>
2021-07-14 16:12:56 +0200

    Add integration tests for azure policies

--- hack/setup-local.sh
+++ hack/setup-local.sh
@@ -1,10 +1,16 @@
 kind create cluster --image quay.io/giantswarm/kind-node:$1 --name kyverno-cluster
 kubectl wait nodes/kyverno-cluster-control-plane --for=condition=ready --timeout=5m > /dev/null
 kind get kubeconfig --name kyverno-cluster > $(pwd)/kube.config
+# Giant Swarm CRDs
 kubectl create --context kind-kyverno-cluster -f https://raw.githubusercontent.com/giantswarm/apiextensions/b9a6bed05850045530eaf8ba7fd01941de6c8525/helm/crds-common/templates/giantswarm.yaml
+# CAPI CRDs
 kubectl create --context kind-kyverno-cluster -f https://raw.githubusercontent.com/kubernetes-sigs/cluster-api/release-0.3/config/crd/bases/cluster.x-k8s.io_clusters.yaml
 kubectl create --context kind-kyverno-cluster -f https://raw.githubusercontent.com/kubernetes-sigs/cluster-api/release-0.3/config/crd/bases/cluster.x-k8s.io_machinedeployments.yaml
+# CAPA CRDs
 kubectl create --context kind-kyverno-cluster -f https://raw.githubusercontent.com/kubernetes-sigs/cluster-api-provider-aws/main/config/crd/bases/infrastructure.cluster.x-k8s.io_awsclusters.yaml
 kubectl create --context kind-kyverno-cluster -f https://raw.githubusercontent.com/kubernetes-sigs/cluster-api-provider-aws/main/config/crd/bases/infrastructure.cluster.x-k8s.io_awsmachinetemplates.yaml
 kubectl create --context kind-kyverno-cluster -f https://raw.githubusercontent.com/kubernetes-sigs/cluster-api-provider-aws/main/config/crd/bases/infrastructure.cluster.x-k8s.io_awsclusterroleidentities.yaml
+# CAPZ CRDs
+kubectl create --context kind-kyverno-cluster -f https://raw.githubusercontent.com/kubernetes-sigs/cluster-api-provider-azure/master/config/crd/bases/infrastructure.cluster.x-k8s.io_azureclusters.yaml
+# Kyverno
 kubectl create --context kind-kyverno-cluster -f https://raw.githubusercontent.com/kyverno/kyverno/main/definitions/release/install.yaml
---
+++ helm/policies-azure/tests/abs/ensure.py
@@ -0,0 +1,119 @@
+import yaml
+from functools import partial
+import time
+import random
+import string
+from textwrap import dedent
+
+import pytest
+from pytest_kube import forward_requests, wait_for_rollout, app_template
+
+import logging
+LOGGER = logging.getLogger(__name__)
+
+cluster_name = "test-cluster"
+release_version = "20.0.0"
+cluster_apps_operator_version = "2.0.0"
+capa_version = "capi"
+
+@pytest.fixture(scope="module")
+def release(kubernetes_cluster):
+    r = dedent(f"""
+        apiVersion: release.giantswarm.io/v1alpha1
+        kind: Release
+        metadata:
+          creationTimestamp: null
+          name: v{release_version}
+        spec:
+          apps:
+          - name: calico
+            version: 0.2.0
+            componentVersion: 3.18.0
+          - name: cert-exporter
+            version: 1.6.0
+          components:
+          - name: cluster-api-bootstrap-provider-kubeadm
+            version: 0.0.1
+          - name: cluster-api-control-plane
+            version: 0.0.1
+          - name: cluster-api-core
+            version: 0.0.1
+          - name: cluster-api-provider-aws
+            version: 0.0.1
+          - name: cluster-apps-operator
+            version: {cluster_apps_operator_version}
+          date: "2021-03-22T14:50:41Z"
+          state: active
+        status:
+          inUse: false
+          ready: false
+    """)
+
+    kubernetes_cluster.kubectl("apply", input=r, output=None)
+    LOGGER.info(f"Release v{release_version} applied")
+
+    yield
+
+    kubernetes_cluster.kubectl(f"delete release v{release_version}", output=None)
+    LOGGER.info(f"Release v{release_version} deleted")
+
+@pytest.fixture
+def cluster(kubernetes_cluster):
+    c = dedent(f"""
+        apiVersion: cluster.x-k8s.io/v1alpha3
+        kind: Cluster
+        metadata:
+          name: {cluster_name}
+          namespace: default
+          labels:
+            release.giantswarm.io/version: {release_version}
+            giantswarm.io/cluster: {cluster_name}
+            cluster.x-k8s.io/cluster-name: {cluster_name}
+        spec:
+          clusterNetwork:
+            pods:
+              cidrBlocks:
+                - 192.168.0.0/16
+          controlPlaneRef:
+            apiVersion: controlplane.cluster.x-k8s.io/v1alpha3
+            kind: KubeadmControlPlane
+            name: {cluster_name}-control-plane
+          infrastructureRef:
+            apiVersion: infrastructure.cluster.x-k8s.io/v1alpha3
+            kind: AzureCluster
+            name: {cluster_name}
+    """)
+
+    kubernetes_cluster.kubectl("apply", input=c, output=None)
+    LOGGER.info(f"Cluster {cluster_name} applied")
+
+    yield
+
+    kubernetes_cluster.kubectl(f"delete cluster {cluster_name}", output=None)
+    LOGGER.info(f"Cluster {cluster_name} deleted")
+
+@pytest.fixture
+def azurecluster(kubernetes_cluster):
+    c = dedent(f"""
+        apiVersion: infrastructure.cluster.x-k8s.io/v1alpha3
+        kind: AzureCluster
+        metadata:
+          name: {cluster_name}
+          namespace: default
+          labels:
+            giantswarm.io/cluster: {cluster_name}
+            cluster.x-k8s.io/cluster-name: {cluster_name}
+    """)
+
+    kubernetes_cluster.kubectl("apply", input=c, output=None)
+    LOGGER.info(f"AzureCluster {cluster_name} applied")
+
+    raw = kubernetes_cluster.kubectl(
+        f"get azurecluster {cluster_name}", output="yaml")
+
+    azurecluster = yaml.safe_load(raw)
+
+    yield azurecluster
+
+    kubernetes_cluster.kubectl(f"delete azurecluster {cluster_name}", output=None)
+    LOGGER.info(f"AzureCluster {cluster_name} deleted")
