4a1257eaa971b124b3daf1061669bd44e291d894
Bricktop <marcel.mueller1@rwth-aachen.de>
2021-07-16 10:50:16 +0200

    Add integration tests for azure policies (#53)
    * Add integration tests for azure policies
    
    * Change variable to watch_label
    
    * Actually rename consistently

---
+++ helm/policies-azure/tests/abs/test_azure_default.py
@@ -0,0 +1,29 @@
+import yaml
+from functools import partial
+import time
+import random
+import string
+import ensure
+from textwrap import dedent
+
+from ensure import release
+from ensure import cluster
+from ensure import azurecluster
+
+import pytest
+from pytest_kube import forward_requests, wait_for_rollout, app_template
+
+import logging
+LOGGER = logging.getLogger(__name__)
+
+
+@pytest.mark.smoke
+def test_azure_cluster_policy(release, cluster, azurecluster) -> None:
+    """
+    test_azure_cluster_policy tests defaulting of an AzureCluster where all required values are empty strings.
+
+    :param release: Release CR which is used by the Cluster.
+    :param cluster: Cluster CR which uses the release and matches the AzureCluster.
+    :param azurecluster: AzureCluster CR with empty strings which matches the Cluster CR.
+    """
+    assert azurecluster['metadata']['labels']['cluster.x-k8s.io/watch-filter'] == ensure.watch_label
