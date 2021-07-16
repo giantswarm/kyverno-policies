17ae522cd00ef69aeb4761d0b7be361951833a2f
Marcel Mueller <marcel.mueller1@rwth-aachen.de>
2021-07-16 14:55:42 +0200

    Add documentation to tests

--- helm/policies-common/tests/abs/test_common_default.py
+++ helm/policies-common/tests/abs/test_common_default.py
@@ -22,10 +22,23 @@ LOGGER = logging.getLogger(__name__)
 
 @pytest.mark.smoke
 def test_cluster_policy(release, cluster) -> None:
+    """
+    test_cluster_policy tests defaulting of a Cluster where all required values are empty strings.
+
+    :param release: Release CR which is used by the Cluster.
+    :param cluster: Cluster CR which uses the release.
+    """
     assert cluster['metadata']['labels']['cluster-apps-operator.giantswarm.io/version'] == ensure.cluster_apps_operator_version
     assert cluster['metadata']['labels']['cluster.x-k8s.io/watch-filter'] == ensure.watch_label
 
 
 @pytest.mark.smoke
 def test_machine_deployment_policy(release, cluster, machinedeployment) -> None:
+    """
+    test_machine_deployment_policy tests defaulting of a MachineDeployment where all required values are empty strings.
+
+    :param release: Release CR which is used by the Cluster.
+    :param cluster: Cluster CR which uses the release.
+    :param machinedeployment: MachineDeployment CR which is referenced by the Cluster.
+    """
     assert machinedeployment['metadata']['labels']['cluster.x-k8s.io/watch-filter'] == ensure.watch_label
