92b309078642104a8e754e9b2368deb20da35514
Christian Bianchi <christian@giantswarm.io>
2021-10-05 14:10:43 +0200

    Keep existing `node-labels` when ensuring the `role=worker` label (#112)
    * Keep existing `node-labels` when ensuring the `role=worker` label exists in `KubeadmConfig`s.
    
    * Keep existing `node-labels` when ensuring the `role=worker` label exists in `KubeadmConfig`s.
    
    * Keep existing `node-labels` when ensuring the `role=worker` label exists in `KubeadmConfig`s.
    
    * Keep existing `node-labels` when ensuring the `role=worker` label exists in `KubeadmConfig`s.

--- helm/tests/ensure.py
+++ helm/tests/ensure.py
@@ -216,6 +216,69 @@ def kubeadmconfig(kubernetes_cluster):
     LOGGER.info(f"KubeadmConfig {cluster_name} deleted")
 
 @pytest.fixture
+def kubeadmconfig_with_labels(kubernetes_cluster):
+    md = dedent(f"""
+        apiVersion: bootstrap.cluster.x-k8s.io/v1alpha3
+        kind: KubeadmConfig
+        metadata:
+          name: {cluster_name}
+          labels:
+            giantswarm.io/cluster: {cluster_name}
+            cluster.x-k8s.io/cluster-name: {cluster_name}
+            cluster.x-k8s.io/watch-filter: {watch_label}
+        spec:
+          joinConfiguration:
+            nodeRegistration:
+              kubeletExtraArgs:
+                node-labels: mylabel=test
+    """)
+
+    kubernetes_cluster.kubectl("apply", input=md, output=None)
+    LOGGER.info(f"KubeadmConfig {cluster_name} applied")
+
+    raw = kubernetes_cluster.kubectl(
+        f"get kubeadmconfig {cluster_name}", output="yaml")
+
+    kubeadmconfig = yaml.safe_load(raw)
+
+    yield kubeadmconfig
+
+    kubernetes_cluster.kubectl(f"delete kubeadmconfig {cluster_name}", output=None)
+    LOGGER.info(f"KubeadmConfig {cluster_name} deleted")
+
+@pytest.fixture
+def kubeadmconfig_with_role_labels(kubernetes_cluster):
+    md = dedent(f"""
+        apiVersion: bootstrap.cluster.x-k8s.io/v1alpha3
+        kind: KubeadmConfig
+        metadata:
+          name: {cluster_name}
+          labels:
+            giantswarm.io/cluster: {cluster_name}
+            cluster.x-k8s.io/cluster-name: {cluster_name}
+            cluster.x-k8s.io/watch-filter: {watch_label}
+        spec:
+          joinConfiguration:
+            nodeRegistration:
+              kubeletExtraArgs:
+                node-labels: role=emperor,mylabel=test
+    """)
+
+    kubernetes_cluster.kubectl("apply", input=md, output=None)
+    LOGGER.info(f"KubeadmConfig {cluster_name} applied")
+
+    raw = kubernetes_cluster.kubectl(
+        f"get kubeadmconfig {cluster_name}", output="yaml")
+
+    kubeadmconfig = yaml.safe_load(raw)
+
+    yield kubeadmconfig
+
+    kubernetes_cluster.kubectl(f"delete kubeadmconfig {cluster_name}", output=None)
+    LOGGER.info(f"KubeadmConfig {cluster_name} deleted")
+
+
+@pytest.fixture
 def kubeadmconfig_controlplane(kubernetes_cluster):
     md = dedent(f"""
         apiVersion: bootstrap.cluster.x-k8s.io/v1alpha3
